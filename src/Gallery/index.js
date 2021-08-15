cbc32fd175e813189d4b5e85cbfa888525a07783
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-15 19:38:20 +0200

    loading animation added

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -2,6 +2,7 @@ import { GalleryWrapper, GalleryHeader, GalleryTilesContainer, GallerySubtitle, 
 import { useGithubApiData } from "./useGithubApiData";
 import Tile from "./Tile";
 import logo from "../githubLogo.svg";
+import Loader from "./Loader";
 
 const Gallery = ({ title, subtitle, body }) => {
     const repos = useGithubApiData();
@@ -16,29 +17,25 @@ const Gallery = ({ title, subtitle, body }) => {
             <GallerySubtitle>
                 {subtitle}
             </GallerySubtitle>
-            <GalleryTilesContainer>
-                {status === "loading" ? (
-                    <div>
-                        Loading...
-                    </div>
-                ) : (status === "error" ? (
-                    <div>
-                        Unable to fetch... please check your internet connection
-                    </div>
-                ) : (
-                    <>
-                        {repos.map((repo) => (
-                            <Tile
-                                key={repo.id}
-                                title={repo.name}
-                                description={repo.description || "n/a"}
-                                demoLink={`https://mcesarczyk.github.io/${repo.name}`}
-                                codeLink={repo.html_url}
-                            />
-                        ))}
-                    </>
-                ))}
-            </GalleryTilesContainer>
+            {status === "loading" ? (
+                <Loader />
+            ) : (status === "error" ? (
+                <div>
+                    Unable to fetch... please check your internet connection
+                </div>
+            ) : (
+                <GalleryTilesContainer>
+                    {repos.map((repo) => (
+                        <Tile
+                            key={repo.id}
+                            title={repo.name}
+                            description={repo.description || "n/a"}
+                            demoLink={`https://mcesarczyk.github.io/${repo.name}`}
+                            codeLink={repo.html_url}
+                        />
+                    ))}
+                </GalleryTilesContainer>
+            ))}
         </GalleryWrapper>
     )
 };
