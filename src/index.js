0c7717fec22cbd0b3f82267504fece2d393c4b9d
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-15 22:59:18 +0200

    switch back to light mode

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkModeTheme}>
+    <ThemeProvider theme={lightModeTheme}>
       <Normalize />
       <GlobalStyle />
       <App />
