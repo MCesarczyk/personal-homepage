22b971ba00e966c8a3be46e5de4c29c18eb9bbcd
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-08-15 16:09:45 +0200

    added hook to get repos data from API

--- src/Gallery/index.js
+++ src/Gallery/index.js
@@ -1,42 +1,48 @@
-import { GalleryWrapper, GalleryHeader, GalleryContainer, GallerySubtitle } from "./styled"
+import { GalleryWrapper, GalleryHeader, GalleryContainer, GallerySubtitle } from "./styled";
+import { useGithubApiData } from "./useGithubApiData";
 import Tile from "./Tile";
 
-const Gallery = ({ title, subtitle, body }) => (
-    <GalleryWrapper>
-        <img alt="" />
-        <GalleryHeader>
-            {title}
-        </GalleryHeader>
-        <GallerySubtitle>
-            {subtitle}
-        </GallerySubtitle>
-        <GalleryContainer>
-            <Tile
-                title={"Repository"}
-                description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut labore voluptates, perspiciatis aliquid rerum ratione veniam tenetur."}
-                demoLink={"https://link.demo.com"}
-                codeLink={"https://link.code.com"}
-            />
-            <Tile
-                title={"Repository"}
-                description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut labore voluptates, perspiciatis aliquid rerum ratione veniam tenetur."}
-                demoLink={"https://link.demo.com"}
-                codeLink={"https://link.code.com"}
-            />
-            <Tile
-                title={"Repository"}
-                description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut labore voluptates, perspiciatis aliquid rerum ratione veniam tenetur."}
-                demoLink={"https://link.demo.com"}
-                codeLink={"https://link.code.com"}
-            />
-            <Tile
-                title={"Repository"}
-                description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut labore voluptates, perspiciatis aliquid rerum ratione veniam tenetur."}
-                demoLink={"https://link.demo.com"}
-                codeLink={"https://link.code.com"}
-            />
-        </GalleryContainer>
-    </GalleryWrapper>
-);
+const Gallery = ({ title, subtitle, body }) => {
+    const repos = useGithubApiData();
+    console.log(repos);
+
+    return (
+        <GalleryWrapper>
+            <img alt="" />
+            <GalleryHeader>
+                {title}
+            </GalleryHeader>
+            <GallerySubtitle>
+                {subtitle}
+            </GallerySubtitle>
+            <GalleryContainer>
+                <Tile
+                    title={"Repository"}
+                    description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut labore voluptates, perspiciatis aliquid rerum ratione veniam tenetur."}
+                    demoLink={"https://link.demo.com"}
+                    codeLink={"https://link.code.com"}
+                />
+                <Tile
+                    title={"Repository"}
+                    description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut labore voluptates, perspiciatis aliquid rerum ratione veniam tenetur."}
+                    demoLink={"https://link.demo.com"}
+                    codeLink={"https://link.code.com"}
+                />
+                <Tile
+                    title={"Repository"}
+                    description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut labore voluptates, perspiciatis aliquid rerum ratione veniam tenetur."}
+                    demoLink={"https://link.demo.com"}
+                    codeLink={"https://link.code.com"}
+                />
+                <Tile
+                    title={"Repository"}
+                    description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aut labore voluptates, perspiciatis aliquid rerum ratione veniam tenetur."}
+                    demoLink={"https://link.demo.com"}
+                    codeLink={"https://link.code.com"}
+                />
+            </GalleryContainer>
+        </GalleryWrapper>
+    )
+};
 
 export default Gallery;
