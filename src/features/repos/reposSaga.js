6ab2d179a2381ae895a6ecb1f77ad2dba1892522
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-10-05 07:01:56 +0200

    error handling added

--- src/features/repos/reposSaga.js
+++ src/features/repos/reposSaga.js
@@ -3,12 +3,16 @@ import { fetchReposData, setRepos, setState } from "../repos/reposSlice";
 import { fetchAPIData } from "./fetchAPIData";
 
 function* fetchReposDataHandler() {
-    yield put(setState("loading"));
-    const apiURL = 'https://api.github.com/users/MCesarczyk/repos?sort="updated"';
-    const repos = yield call(fetchAPIData, apiURL);
-    yield put(setRepos(repos));
-    yield delay(2_000);
-    yield put(setState("success"));
+    try {
+        yield put(setState("loading"));
+        const apiURL = 'https://api.github.com/users/MCesarczyk/repos?sort="updated"';
+        const repos = yield call(fetchAPIData, apiURL);
+        yield put(setRepos(repos));
+        yield delay(2_000);
+        yield put(setState("success"));
+    } catch {
+        yield put(setState("error"));
+    }
 };
 
 export function* reposSaga() {
