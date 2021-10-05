d626a23318cce829211fd514ca52d651e53d3ef8
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-10-05 06:57:50 +0200

    fetching data from Github API

--- src/features/repos/reposSaga.js
+++ src/features/repos/reposSaga.js
@@ -1,8 +1,14 @@
-import { put, takeLatest } from "@redux-saga/core/effects";
-import { fetchReposData, setState } from "../repos/reposSlice";
+import { call, delay, put, takeLatest } from "@redux-saga/core/effects";
+import { fetchReposData, setRepos, setState } from "../repos/reposSlice";
+import { fetchAPIData } from "./fetchAPIData";
 
 function* fetchReposDataHandler() {
     yield put(setState("loading"));
+    const apiURL = 'https://api.github.com/users/MCesarczyk/repos?sort="updated"';
+    const repos = yield call(fetchAPIData, apiURL);
+    yield put(setRepos(repos));
+    yield delay(2_000);
+    yield put(setState("success"));
 };
 
 export function* reposSaga() {
