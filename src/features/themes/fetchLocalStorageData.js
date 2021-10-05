c9372ead3984dbd0b58a300eeccfea2ee6947e32
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-10-05 07:17:01 +0200

    fetching theme from local storage

---
+++ src/features/themes/fetchLocalStorageData.js
@@ -0,0 +1,8 @@
+export const fetchLocalStorageData = async (keyName, initialValue) => {
+        const localStorageState = await localStorage.getItem(keyName);
+        if (localStorageState === null) {
+            return await initialValue;
+        }
+
+        return await JSON.parse(localStorage.getItem(keyName));
+};
--- src/features/themes/themeSaga.js
+++ src/features/themes/themeSaga.js
@@ -1,8 +1,10 @@
 import { call, put, takeLatest } from "@redux-saga/core/effects";
+import { fetchLocalStorageData } from "./fetchLocalStorageData";
 import { fetchThemeState, setTheme } from "./themeSlice";
 
 function* fetchThemeStateHandler() {
-    console.log("themeSaga");
+    const theme = yield call(fetchLocalStorageData, "theme", "lightMode");
+    console.log(theme);
 };
 
 export function* themeSaga() {
