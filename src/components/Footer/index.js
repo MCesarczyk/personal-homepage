6980d81fd5c051b0a0af2c01d9ac49603d2459cf
Michał Cesarczyk <83251458+MCesarczyk@users.noreply.github.com>
2021-10-06 09:57:21 +0200

    changed footer thumbnails into array

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,6 @@
 import { FooterContainer, FooterHeader, FooterLink, FooterParagraph, FooterThumbnail, FooterThumbnailLink, FooterThumbnailWrapper } from "./styled";
-import { ReactComponent as GithubThumbnail } from "../../assets/images/githubThumbnail.svg";
-import { ReactComponent as FacebookThumbnail } from "../../assets/images/facebookThumbnail.svg";
-import { ReactComponent as LinkedinThumbnail } from "../../assets/images/linkedinThumbnail.svg";
-import { ReactComponent as InstagramThumbnail } from "../../assets/images/instagramThumbnail.svg";
 import { Caption } from "../../common/Caption";
+import { thumbnails } from "./thumbnails";
 
 const Footer = () => (
     <FooterContainer>
@@ -19,33 +16,14 @@ const Footer = () => (
             I'm open to new challenging tasks, so when you need website or one page application to launch, feel free to talk to me.
         </FooterParagraph>
         <FooterThumbnailWrapper>
-            <FooterThumbnail>
-                <FooterThumbnailLink href="https://github.com/MCesarczyk" target="_blank" >
-                    <GithubThumbnail />
-                </FooterThumbnailLink>
-            </FooterThumbnail>
-            <FooterThumbnail>
-                <FooterThumbnailLink href="https://www.facebook.com/mich.cesarz/" target="_blank" >
-                    <FacebookThumbnail />
-                </FooterThumbnailLink>
-            </FooterThumbnail>
-            <FooterThumbnail>
-                <FooterThumbnailLink href="https://www.linkedin.com/in/micha%C5%82-cesarczyk-0820589a/" target="_blank" >
-                    <LinkedinThumbnail />
-                </FooterThumbnailLink>
-            </FooterThumbnail>
-            <FooterThumbnail>
-                <FooterThumbnailLink href="https://www.instagram.com/mich_cesarz/" target="_blank" >
-                    <InstagramThumbnail />
-                </FooterThumbnailLink>
-            </FooterThumbnail>
+            {thumbnails.map(({ id, thumbnail, url }) => (
+                <FooterThumbnail key={id}>
+                    <FooterThumbnailLink href={url} target="_blank" rel="noopener noreferrer" >
+                        {thumbnail}
+                    </FooterThumbnailLink>
+                </FooterThumbnail>
+            ))}
         </FooterThumbnailWrapper>
-        {/* {thumbnails.map(({ id, thumbnail, url }) => {
-                <FooterThumbnailWrapper key={id}>
-                    {thumbnail}
-                    <a href={url} target="_blank" rel="noopener noreferrer" />
-                </FooterThumbnailWrapper>
-            })} */}
     </FooterContainer>
 );
 
---
+++ src/components/Footer/thumbnails.js
@@ -0,0 +1,27 @@
+import { ReactComponent as GithubThumbnail } from "../../assets/images/githubThumbnail.svg";
+import { ReactComponent as FacebookThumbnail } from "../../assets/images/facebookThumbnail.svg";
+import { ReactComponent as LinkedinThumbnail } from "../../assets/images/linkedinThumbnail.svg";
+import { ReactComponent as InstagramThumbnail } from "../../assets/images/instagramThumbnail.svg";
+
+export const thumbnails = [
+    {
+        id: 1,
+        thumbnail: <GithubThumbnail />,
+        url: "https://github.com/MCesarczyk",
+    },
+    {
+        id: 1,
+        thumbnail: <FacebookThumbnail />,
+        url: "https://www.facebook.com/mich.cesarz/",
+    },
+    {
+        id: 1,
+        thumbnail: <LinkedinThumbnail />,
+        url: "https://www.linkedin.com/in/micha%C5%82-cesarczyk-0820589a/",
+    },
+    {
+        id: 1,
+        thumbnail: <InstagramThumbnail />,
+        url: "https://www.instagram.com/mich_cesarz/",
+    },
+];
