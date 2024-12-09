8f01fb6d18b09c728db9f31940e55ca1bb076eb5
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-12-10 00:37:59 +0100

    ci: disable token in production

--- src/utils/fetchAPIData.ts
+++ src/utils/fetchAPIData.ts
@@ -1,8 +1,11 @@
 export const fetchAPIData = async (apiURL: string) => {
     const response = await fetch(apiURL, {
         method: "GET",
-        headers: {
+        headers: process.env.NODE_ENV === "development" ? {
             "Authorization": `Bearer ${process.env.REACT_APP_GITHUB_API_TOKEN}`,
+            "Content-Type": "application/json",
+            "X-GitHub-Api-Version": "2022-11-28"
+        } : {
             "Content-Type": "application/json",
             "X-GitHub-Api-Version": "2022-11-28"
         },
