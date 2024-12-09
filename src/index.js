cdc635d2c4df80a906cf4a1e2e337980899f9632
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-12-09 08:54:17 +0100

    build: bump react to 19, update scripts, add commitizen

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from "./store";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
