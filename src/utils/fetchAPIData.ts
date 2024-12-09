76b56abd3c06a661ee6e984ebdb09277eca7650a
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-12-10 00:48:58 +0100

    ci: hide github token before deployment

--- src/utils/fetchAPIData.ts
+++ src/utils/fetchAPIData.ts
@@ -1,11 +1,8 @@
 export const fetchAPIData = async (apiURL: string) => {
     const response = await fetch(apiURL, {
         method: "GET",
-        headers: process.env.NODE_ENV === "development" ? {
-            "Authorization": `Bearer ${process.env.REACT_APP_GITHUB_API_TOKEN}`,
-            "Content-Type": "application/json",
-            "X-GitHub-Api-Version": "2022-11-28"
-        } : {
+        headers: {
+            // "Authorization": `Bearer ${process.env.REACT_APP_GITHUB_API_TOKEN}`, // add your GitHub API token here
             "Content-Type": "application/json",
             "X-GitHub-Api-Version": "2022-11-28"
         },
